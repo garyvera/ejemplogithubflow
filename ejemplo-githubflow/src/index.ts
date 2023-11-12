916aa7bf336263b2743926a3932326f33d460e44
Gary Vera <garyveralucas@gmail.com>
2023-11-12 08:55:54 -0500

    No ticket: Agregar resta

--- ejemplo-githubflow/src/index.ts
+++ ejemplo-githubflow/src/index.ts
@@ -1,6 +1,7 @@
 export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
   return a + b;
+};
+
+export const substract = (a: number, b: number) => {
+  return a - b;
 };
