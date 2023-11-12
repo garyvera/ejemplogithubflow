b1525156fe58772c8a5cdbcd40763bdd22afa255
garyvera <108149433+garyvera@users.noreply.github.com>
2023-11-12 09:28:51 -0500

    No ticket: agrego test de substract (#3)

--- ejemplo-githubflow/test/blah.test.ts
+++ ejemplo-githubflow/test/blah.test.ts
@@ -1,7 +1,13 @@
-import { sum } from '../src';
+import { substract, sum } from '../src';
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('blah', () => {
+  it('works', () => {
+    expect(substract(2, 1)).toEqual(1);
   });
 });
